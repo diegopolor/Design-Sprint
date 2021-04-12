99fe8b9026ee4711fb5c1303502e6322ed060847
Camilo Ternera <camiloterneraduque.gmail.com>
2021-04-12 16:35:06 -0500

    probando primer push con compañero

--- js/main.js
+++ js/main.js
@@ -1,18 +1,7 @@
-
 //función que hace que cambie de tamaño el menu de arriba al hacer scroll
 
 const menu = document.querySelector(".navbar-logo");
-document.addEventListener("scroll", ()=>{
-
-         if(scrollY > 267) menu.style.width = "50px";                     
-    else if(scrollY < 267) menu.style.width = "60px";
-
-    
-
-})
-
-
-
-
-
-    
+document.addEventListener("scroll", () => {
+  if (scrollY > 267) menu.style.width = "50px";
+  else if (scrollY < 267) menu.style.width = "60px";
+});
